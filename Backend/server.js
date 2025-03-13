dd239126f6a4cbbe481de94aa060ce2fc882fe91
ayushgit12 <ayush2005raj@gmail.com>
2025-03-14 00:50:03 +0530

    / route backend

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
 app.use("/api", routes);
 
 connectToMongoDB()
