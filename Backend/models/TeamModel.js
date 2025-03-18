080481f989d875c969c295595ff4ea8502389b46
Vaibhaviitian <aryanvaibhav74@gmail.com>
2025-03-18 21:41:36 +0530

    adding ref

--- Backend/models/TeamModel.js
+++ Backend/models/TeamModel.js
@@ -7,19 +7,23 @@ const MemberSchema = new mongoose.Schema({
   email: { type: String, required: true },
 });
 
-const TeamSchema = new mongoose.Schema({
-  teamName: { type: String, required: true },
-  leaderName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  leaderCollege: { type: String, required: true },
-  leaderYear: { type: String, required: true },
-  memberCount: { type: Number, required: true },
-  members: { type: [MemberSchema]},
-  idProofUrl: { type: String, required: true },
-  uniqueId: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  status: { type: String, required: true, default: 'pending' },
-}, { timestamps: true });
+const TeamSchema = new mongoose.Schema(
+  {
+    teamName: { type: String, required: true },
+    leaderName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    leaderCollege: { type: String, required: true },
+    leaderYear: { type: String, required: true },
+    memberCount: { type: Number, required: true },
+    members: { type: [MemberSchema] },
+    idProofUrl: { type: String, required: true },
+    uniqueId: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    status: { type: String, required: true, default: "pending" },
+    referral: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
 
 const Team = mongoose.model("Team", TeamSchema);
 export default Team;
