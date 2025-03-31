ec5362671233673a05975f208b47a0f7cabad1ca
Anubhaw6557 <aanubhaw0@gmail.com>
2025-03-31 12:27:20 +0530

    added ps9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,7 @@ import Ps5 from "./Components/problemstatemant/ps5.jsx";
 import Ps6 from "./Components/problemstatemant/ps6.jsx";
 import Ps7 from "./Components/problemstatemant/ps7.jsx";
 import Ps8 from "./Components/problemstatemant/ps8.jsx";
+import Ps9 from "./Components/problemstatemant/ps9.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -46,6 +47,7 @@ function App() {
           <Route path="/problemstatement/ps6" element={<Ps6 />} />
           <Route path="/problemstatement/ps7" element={<Ps7 />} />
           <Route path="/problemstatement/ps8" element={<Ps8 />} />
+          <Route path="/problemstatement/ps9" element={<Ps9 />} />
         </Routes>
       </BrowserRouter>
     </>
