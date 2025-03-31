479d09fbb3a7af804342083ddd5e2de1e58d90b4
Vaibhaviitian <aryanvaibhav74@gmail.com>
2025-04-01 00:18:44 +0530

    addig

--- admin/server/controllers/teamControllers.js
+++ admin/server/controllers/teamControllers.js
@@ -1,13 +1,15 @@
 // controllers/teamController.js
-import Team from '../models/TeamModel.js'; // Import the Team model
+import Team from "../models/TeamModel.js"; // Import the Team model
 
 // middleware/passwordCheckMiddleware.js
 
 export const checkPassword = (req, res, next) => {
   const { password } = req.body;
   if (!process.env.pass) {
-    console.error('Server Error: Password environment variable not set');
-    return res.status(500).json({ success: false, message: 'Server configuration error' });
+    console.error("Server Error: Password environment variable not set");
+    return res
+      .status(500)
+      .json({ success: false, message: "Server configuration error" });
   }
 
   const isValidPassword = (password) => {
@@ -15,7 +17,9 @@ export const checkPassword = (req, res, next) => {
   };
 
   if (!password || !isValidPassword(password)) {
-    return res.status(401).json({ success: false, message: 'Invalid password' });
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid password" });
   }
 
   next();
@@ -27,12 +31,18 @@ export const getAllTeams = async (req, res) => {
     const teams = await Team.find({});
     res.status(200).json({ success: true, data: teams });
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Failed to fetch teams', error: error.message });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Failed to fetch teams",
+        error: error.message,
+      });
   }
 };
 export const login = async (req, res) => {
   const teams = await Team.find({});
-    res.status(200).json({ success: true,data: teams  });
+  res.status(200).json({ success: true, data: teams });
 };
 
 // Update team status
@@ -49,21 +59,31 @@ export const updateTeamStatus = async (req, res) => {
     );
 
     if (!updatedTeam) {
-      return res.status(404).json({ success: false, message: 'Team not found' });
+      return res
+        .status(404)
+        .json({ success: false, message: "Team not found" });
     }
 
     res.status(200).json({ success: true, data: updatedTeam });
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Failed to update team status', error: error.message });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Failed to update team status",
+        error: error.message,
+      });
   }
 };
-
 
 export const teamOut = async (req, res) => {
+  console.log("teamout");
+
   try {
-    const { scannedData, count } = req.body;
-    uniqueId=scannedData.uniqueId;
-    password=scannedData.password;
+    const { parsedData, count } = req.body;
+    console.log(parsedData, count);
+    let password = parsedData.password;
+    let uniqueId = parsedData.teamId;
 
     const team = await Team.findOne({ uniqueId, password });
 
@@ -76,7 +96,7 @@ export const teamOut = async (req, res) => {
       {
         outTime: {
           date: currentDate,
-          count: team.outTime.count+count,
+          count: team.outTime.count + count,
         },
       },
       { new: true } // Return the updated document
@@ -87,29 +107,30 @@ export const teamOut = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-
 
 export const teamIn = async (req, res) => {
+  console.log("teamin");
   try {
-    const { scannedData,count } = req.body;
-    uniqueId = scannedData.uniqueId;
-    password = scannedData.password;
+    const { parsedData, count } = req.body;
+    console.log(parsedData, count);
+    let uniqueId = parsedData.teamId;
+    let password = parsedData.password;
     const team = await Team.findOne({ uniqueId, password });
 
     if (!team) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
+    console.log(team);
     const currentDate = new Date();
-    const diffInMinutes = team.count*(currentDate - team.outTime.date) ;
+    const diffInMinutes = team.outTime.count * (currentDate - team.outTime.date);
     await Team.findOneAndUpdate(
       { uniqueId },
       {
         outTime: {
           date: currentDate,
-          count: team.outTime.count-count,
+          count: team.outTime.count - count,
         },
         totalTime: team.totalTime + diffInMinutes,
-
       },
       { new: true } // Return the updated document
     );
@@ -120,26 +141,27 @@ export const teamIn = async (req, res) => {
   }
 };
 
-export const runEveryMinute = async() => {
-  setInterval(async() => {
-    const teams= await Team.find({});
+export const runEveryMinute = async () => {
+  setInterval(async () => {
+    const teams = await Team.find({});
     teams.forEach(async (team) => {
-      const { outTime,count } = team;
+      const { outTime, count } = team;
       if (outTime.date) {
         const newOutTime = new Date();
-        const diffInMinutes = count*(newOutTime - outTime.date);
-        if (diffInMinutes >=5000) {
+        const diffInMinutes = count * (newOutTime - outTime.date);
+        if (diffInMinutes >= 5000) {
           await Team.findOneAndUpdate(
             { uniqueId: team.uniqueId },
-            { outTime: { date: newOutTime, count: 0 } ,
-               totalTime: team.totalTime + diffInMinutes }
+            {
+              outTime: { date: newOutTime, count: 0 },
+              totalTime: team.totalTime + diffInMinutes,
+            }
           );
         }
-      }
-      else{
+      } else {
         await Team.findOneAndUpdate(
           { uniqueId: team.uniqueId },
-          { outTime: { date: new Date(), count: 0 }, }
+          { outTime: { date: new Date(), count: 0 } }
         );
       }
     });
